7571e34ec796f10afc31666d43ada5b5dae29a73
Ittichai Boonyarakthunya <i.boonyarakthunya@gmail.com>
2025-05-16 10:06:14 +0700

    adding store constant file and ignore test step on CI

---
+++ utils/constant.py
@@ -0,0 +1,21 @@
+# News API
+NEWS_API_BASE_URL = "https://api.thenewsapi.com/v1/news/top"
+NEWS_API_LANGUAGE = "en"
+NEWS_API_COUNTRIES = "us"
+NEWS_API_CATEGORIES = "tech"
+
+# Huggingface Model
+HF_MODEL_NAME = "facebook/bart-large-cnn"
+
+# Discord
+DISCORD_USERNAME = "News Digest Bot"
+DISCORD_AVATAR_URL = "https://i.imgur.com/xyz1234.png"
+
+# Pinecone
+PINECONE_INDEX_NAME = "news-digest-index"
+PINECONE_NAMESPACE = "news"
+
+# ENV & Paths
+ENV_PATH = ".env"
+LOG_DIR = "data/logs"
+DEFAULT_SUMMARY_LENGTH = 3
